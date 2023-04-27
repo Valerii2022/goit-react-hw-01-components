373cb54644c77e9ff32e1f53e55045139aaf6876
Valerii Pometun <vmpometun@gmail.com>
2023-04-27 23:22:55 +0300

    done task #1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import user from './user.json';
 export const App = () => {
   return (
     <>
-      <Profile user="{user}" />
+        <Profile username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} />
     </>
   );
 };
