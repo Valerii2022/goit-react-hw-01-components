93bc6f0a30acbc956e6bcfd5e10aea4ce2f58dd7
Valerii Pometun <vmpometun@gmail.com>
2023-04-27 23:33:21 +0300

    change some files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,8 @@
 import { Profile } from './Profile/Profile';
-import user from './user.json';
+import user from '../data/user.json';
+// import data from '../data/data.json';
+// import friends from '../data/friends.json';
+// import transactions from '../data/transactions.json';
 
 export const App = () => {
   return (
