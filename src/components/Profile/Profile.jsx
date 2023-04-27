0ed3931292dc106a706646eb4466fc17db64cb66
Valerii Pometun <vmpometun@gmail.com>
2023-04-27 17:32:58 +0300

    start working

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,6 +1,14 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 // import css from './Profile.module.css';
 
-// export const Profile = () => {};
+export const Profile = user => {
+  return (
+    <div className="profile__wrap">
+      <p>{user.username}</p>
+    </div>
+  );
+};
 
-// Profile.propTypes = {};
+Profile.propTypes = {
+  username: PropTypes.string,
+};
