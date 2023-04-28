097fb9eb7523bfaefed933c4db7e53a4899bc990
Valerii Pometun <vmpometun@gmail.com>
2023-04-28 11:56:05 +0300

    task #2 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,21 @@
 import { Profile } from './Profile/Profile';
 import user from '../data/user.json';
-// import data from '../data/data.json';
+import { Statistics } from './Statistics/Statistics';
+import data from '../data/data.json';
 // import friends from '../data/friends.json';
 // import transactions from '../data/transactions.json';
 
 export const App = () => {
   return (
     <>
-        <Profile username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
     </>
   );
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,6 +1,38 @@
-// import PropTypes from 'prop-types';
-// import css from './Statistics.module.css';
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
+import { getRandomColor } from '../../utils/getRandomColor';
 
-export const Statistics = () => {};
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
 
-// Statistics.propTypes = {};
+      <ul className={css.stat__list}>
+        {stats.map(el => {
+          const backgroundColor = getRandomColor();
+          return (
+            <li
+              className={css.item}
+              key={el.id}
+              style={{ backgroundColor: [backgroundColor] }}
+            >
+              <span className={css.label}>{el.label}</span>
+              <span className={css.percentage}>{el.percentage}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
+};
