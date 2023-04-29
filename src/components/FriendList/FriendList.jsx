1e6ac4b253874ead5967e16decbb144fc5d8e0de
Valerii Pometun <vmpometun@gmail.com>
2023-04-29 23:52:15 +0300

    task #3

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,8 +1,13 @@
 // import PropTypes from 'prop-types';
 // import css from './FriendList.module.css';
+import { FriendListItem } from "../FriendListItem/FriendListItem";
 
 export const FriendList = ({ friends }) => {
-  return <ul className="friend-list"></ul>;
+   return <ul className="friend-list">
+      {friends.map((el) => {
+         <FriendListItem avatar={friends[0].avatar} name={friends[0].name} isOnline={friends[0].isOnline}/>
+      })}
+  </ul>;
 };
 
 // FriendList.propTypes = {};
--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -5,9 +5,9 @@
 export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <li className="item">
-      <span className="status"></span>
-      <img className="avatar" src="" alt="User avatar" width="48" />
-      <p className="name"></p>
+        <span className="status">isOnline{isOnline}</span>
+      <img className="avatar" src={avatar} alt="User avatar" width="48" />
+        <p className="name">{name}</p>
     </li>
   );
 };
