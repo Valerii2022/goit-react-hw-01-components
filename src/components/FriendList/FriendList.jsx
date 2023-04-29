87ea0c5c79224e35c9a4fd224e2cc07a94cc027a
Valerii Pometun <vmpometun@gmail.com>
2023-04-30 00:01:04 +0300

    changes

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -5,11 +5,12 @@ import { FriendListItem } from "../FriendListItem/FriendListItem";
 export const FriendList = ({ friends }) => {
    return <ul className="friend-list">
       {friends.map((el) => {
-         <li className="item">
-        <span className="status">isOnline{el.isOnline}</span>
-      <img className="avatar" src={el.avatar} alt="User avatar" width="48" />
-        <p className="name">{el.name}</p>
-    </li>
+         console.log(el)
+   //       <li className="item">
+   //      <span className="status">isOnline{el.isOnline}</span>
+   //    <img className="avatar" src={el.avatar} alt="User avatar" width="48" />
+   //      <p className="name">{el.name}</p>
+   //  </li>
          // <FriendListItem avatar={friends[0].avatar} name={friends[0].name} isOnline={friends[0].isOnline}/>
       })}
   </ul>;
