255a550c7a1aab4ee1f785239cc0bca394926c65
Valerii Pometun <vmpometun@gmail.com>
2023-04-30 00:28:27 +0300

    task #3

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -2,13 +2,14 @@
 // import css from './FriendListItem.module.css';
 // import friends from './friends.json';
 
-export const FriendListItem = ({ avatar, name, isOnline }) => {
-  return (
-    <li className="item">
-        <span className="status">isOnline{isOnline}</span>
-      <img className="avatar" src={avatar} alt="User avatar" width="48" />
-        <p className="name">{name}</p>
-    </li>
+export const FriendListItem = ({ title }) => {
+   return (
+     console.log(title)
+   //  <li className="item">
+   //      <span className="status">isOnline{isOnline}</span>
+   //    <img className="avatar" src={avatar} alt="User avatar" width="48" />
+   //      <p className="name">{name}</p>
+   //  </li>
   );
 };
 
