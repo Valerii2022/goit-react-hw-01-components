41b08ff7f2a8f7c95363cea8f982f2d3a81fb2c2
Valerii Pometun <vmpometun@gmail.com>
2023-05-01 23:15:25 +0300

    task #4 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,10 @@ import { Profile } from './Profile/Profile';
 import user from '../data/user.json';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 import data from '../data/data.json';
 import friends from '../data/friends.json';
-// import friends from '../data/friends.json';
-// import transactions from '../data/transactions.json';
+import transactions from '../data/transactions.json';
 
 export const App = () => {
   return (
@@ -18,7 +18,8 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends} />
+        <FriendList friends={friends} />
+        <TransactionHistory items={transactions} />
     </>
   );
 };
--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,6 +1,39 @@
-// import PropTypes from 'prop-types';
-// import css from './TransactionHistory.module.css';
+import PropTypes from 'prop-types';
+import css from './TransactionHistory.module.css';
 
-// export const TransactionHistory = () => {};
+export const TransactionHistory = ({ items }) => {
+  return (
+    <table className={css.transaction__history}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
 
-// TransactionHistory.propTypes = {};
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => {
+          return (
+            <tr key={id}>
+              <td>{type}</td>
+              <td>{amount}</td>
+              <td>{currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
+};
